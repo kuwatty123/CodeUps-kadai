427c864065d4f0553b227e1ac06ff9d0aea0a36d
kuwatty <128878724+kuwatty@users.noreply.github.com>
2024-02-19 20:06:53 +0900

    commit_0219

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  //ドロワーメニュー
+  $(".js-hamburger").on('click', function () {
+    if ($('.js-hamburger').hasClass('is-active')) {
+      $('.js-hamburger').removeClass("is-active");
+      // $("html").toggleClass("is-fixed");
+      $('.js-sp-nav').fadeOut(300);
+      console.log('フェードアウト');
+    } else {
+      $('.js-hamburger').addClass("is-active");
+      // $("html").toggleClass("is-fixed");
+      $('.js-sp-nav').fadeIn(300);
+      console.log('フェードイン');
+    }
+  });
+
+  //スライダー
+  $('.mv__slider').slick({
+    autoplay: true, // 自動再生オン
+    speed: 1000, // 画像切り替え速度
+    fade: true, // フェードで画像が切り替わる
+});
+
+
+
+});
