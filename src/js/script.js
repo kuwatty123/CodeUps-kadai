f7a6e80fd264a2a49475086489493cc91a685234
kuwatty <128878724+kuwatty@users.noreply.github.com>
2024-02-19 20:16:56 +0900

    commit_0219-2

--- src/js/script.js
+++ src/js/script.js
@@ -18,7 +18,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
   //スライダー
   $('.mv__slider').slick({
     autoplay: true, // 自動再生オン
-    speed: 1000, // 画像切り替え速度
+    speed: 1500, // 画像切り替え速度
     fade: true, // フェードで画像が切り替わる
 });
 
