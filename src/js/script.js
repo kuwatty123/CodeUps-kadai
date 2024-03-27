10d02935a76260324d2d13398b4796c476caa26a
kuwatty123 <webenginkuwa123@gmail.com>
2024-03-27 20:19:01 +0900

    0327-1

--- src/js/script.js
+++ src/js/script.js
@@ -15,7 +15,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     }
   });
 
-  //スライダー
+  //スライダー メインビュー
   $('.mv__slider').slick({
     arrows: false,//矢印を非表示にする
     autoplay: true, // 自動再生オン
@@ -23,6 +23,65 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     fade: true, // フェードで画像が切り替わる
 });
 
+//スライダー キャンペーン
+$(".campaign__slider + pc-slider").slick({
+  autoplay: true,
+  autoplaySpeed:2000,
+  speed:2000,
+  arrows: true,
+  slidesToShow: 4,
+  slidesToScroll: 1
+});
+
+$(".campaign__slider + sp-slider").slick({
+  autoplay: true,
+  autoplaySpeed:2000,
+  speed:2000,
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
 
 
+
+});
+
+const swiper = new Swiper('.swiper', {
+  speed: 1000,
+  autoplay: {
+    delay: 3000, // 途切れなくループ
+  },
+
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  slidesPerView: 3, //スライダーのコンテナ上に2枚同時に表示
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    type: 'bullets',
+		clickable: true,
+    
+  },
+
+  spaceBetween: 10 ,//スライド感の余白
+
+
+});
+
+swiper.on('slideChange', function () {
+  console.log('slide changed');
 });
